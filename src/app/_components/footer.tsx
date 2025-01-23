fda6ea31239bf9326be68c04044ee3623284bc4a
shibamoto <silverbirder@gmail.com>
2025-01-23 23:25:26 +0900

    chore: add link

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -7,9 +7,17 @@ export const Footer = () => {
     <footer className="my-16 w-full border-t py-4">
       <div className="mx-auto flex flex-row items-center justify-between px-4 text-xs text-foreground">
         <p>© {currentYear} silverbirder. All rights reserved.</p>
-        <Link href="https://forms.gle/RkrK3cHXDnbmPEfe7" target="_blank">
-          お問い合わせ
-        </Link>
+        <nav className="flex items-center gap-4">
+          <Link href="https://forms.gle/RkrK3cHXDnbmPEfe7" target="_blank">
+            お問い合わせ
+          </Link>
+          <Link
+            href="https://sites.google.com/view/silverbirders-services"
+            target="_blank"
+          >
+            関連サービス
+          </Link>
+        </nav>
       </div>
     </footer>
   );
